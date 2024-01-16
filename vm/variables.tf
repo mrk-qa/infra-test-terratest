93cc9dbfcefc259a187fdadbdae1026cd3f5be58
Marco Antonio <marco.ant.seg@gmail.com>
2024-01-16 02:19:05 -0300

    without key pub

--- vm/variables.tf
+++ vm/variables.tf
@@ -1,7 +1,6 @@
 variable "aws_key_pub" {
   description = "Chave pública para a VM na AWS"
   type        = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCk93XNluP3vFBjfoK/CjtRnW+SLqFe+7DHN+fGnoUbyNdWViQBJ+Vl3fgL/1nD6WihfP8cRUucZ3n9U6ih2f4n6uIHS7BlI2iVDrdfj5W2HG3Jm2gZSVgSazcqJ36VOWXRQAeBIiLcdCt5m5W9drEL69EkFKpYF1cyZeaVD1/ogbero+VERfEZ8H8CC4jsV0CraN4Cw2vunqqV2t0rdyCqrSQUgArgoqd6hCCmcTeWQOXQzLolTnEtJ/R33JnDqC5IXt5LBvmmjILn0jgswFGWkat2LgM0Ts4Fcdr1UnAhvCzPKtbKB2n4R5ettXQ9f1DubfUubyDbviKr6V5/geBx marco-qa@MRK"
 }
 
 variable "key_pair_name_aws" {
