482d4c1714818bb8948b4941fe36595a451fa5dc
Marco Antonio <marco.ant.seg@gmail.com>
2024-01-16 02:26:50 -0300

    clean

--- tests/terraform-aws-vm_test.go
+++ tests/terraform-aws-vm_test.go
@@ -20,10 +20,8 @@ func TestTerraformAwsInstance(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
-	// website::tag::4:: Run `terraform output` to get the IP of the instance
 	publicIp := terraform.Output(t, terraformOptions, "public_ip")
 
-	// website::tag::5:: Make an HTTP request to the instance and make sure we get back a 200 OK with the body "Hello, World!"
 	url := fmt.Sprintf("http://%s:80", publicIp)
 	http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, Marco!", 30, 5*time.Second)
 }
