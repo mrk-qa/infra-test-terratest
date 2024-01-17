399872279bf197dadb602cc31edc89414c924b04
Marco Antonio <marco.ant.seg@gmail.com>
2024-01-17 20:35:11 -0300

    add resource lambda with apigateway

---
+++ resources/lambda/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "Region of resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_account_id" {
+  description = "Region of resources"
+  type        = string
+  default     = "162256499619"
+}
+
+variable "function_name" {
+  description = "The name of the function to provision"
+  type        = string
+  default     = "api_lambda"
+}
+
+variable "application_name" {
+  description = "Used to name resources provisioned by this template"
+  type        = string
+  default     = "terraform-and-terratest-aws"
+}
