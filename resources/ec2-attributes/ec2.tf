c7c876d0c8df3df3647115c70d275764640a00f0
Marco Antonio <marco.ant.seg@gmail.com>
2024-01-19 17:44:54 -0300

    add resource ec2

---
+++ resources/ec2-attributes/ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_key_pair" "key" {
+  key_name   = var.key_pair_name_aws
+  public_key = var.aws_key_pub
+}
+
+resource "aws_instance" "vm" {
+  ami                         = var.ami_vm
+  instance_type               = var.instance_type_vm
+  key_name                    = aws_key_pair.key.key_name
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id_aws_full
+  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id_aws_full]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = var.application_name
+  }
+}
---
+++ resources/ec2-attributes/variables.tf
@@ -0,0 +1,33 @@
+variable "aws_key_pub" {
+  description = "Chave pública para a VM na AWS"
+  type        = string
+  sensitive   = true
+}
+
+variable "key_pair_name_aws" {
+  description = "Nome da key pair usada na AWS"
+  type        = string
+  default     = "aws-key-infra-test-terratest"
+}
+
+variable "ami_vm" {
+  description = "AMI to be used to run the VM instance"
+  type        = string
+  default     = "ami-0c7217cdde317cfec"
+}
+
+variable "instance_type_vm" {
+  description = "Instance type to run the VM instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "tag_id_aws" {
+  description = "Tag usado na AWS"
+  type        = string
+  default     = "terraform-with-terratest"
+}
+
+variable "application_name" {
+  default = "ec2-attributes-test"
+}
