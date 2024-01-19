898676b2a4943c49de87b1b121a438ccd46c126e
Marco Antonio <marco.ant.seg@gmail.com>
2024-01-19 18:43:39 -0300

    removed key_name

--- resources/ec2-attributes/ec2.tf
+++ resources/ec2-attributes/ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "vm" {
   ami                         = var.ami_vm
   instance_type               = var.instance_type_vm
-  key_name                    = aws_key_pair.key.key_name
   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id_aws_full
   vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id_aws_full]
   associate_public_ip_address = true
